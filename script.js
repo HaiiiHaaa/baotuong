13bf77734594efefb67cabcbebc792ab7b91cb6d
HaiiiHaaa <ha180407240607@gmail.com>
2025-02-08 14:35:09 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,1 @@
-document.body.addEventListener("click", function(event) {
-    let firework = document.createElement("div");
-    firework.classList.add("firework");
-    firework.style.left = event.clientX + "px";
-    firework.style.top = event.clientY + "px";
-    document.body.appendChild(firework);setTimeout(() => {firework.remove();}, 1000);});
+
